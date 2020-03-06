d9105435c351b1572d0879dbac644064079b16b5
Lyleof <finn.jensen@mymail.champlain.edu>
2020-03-06 04:36:50 -0500

    Modling Code

--- data_modeling.py
+++ data_modeling.py
@@ -1,4 +1,4 @@
-# Author: Gale Proulx & Finn Jensen
+# Author: Gale Proulx and Finn Jensen
 # Class:  DAT-310-01
 # Certification of Authenticity:
 # I certify that this is my work and the DAT-330 class work,
@@ -15,32 +15,82 @@
 # IMPORT DEPENDENCIES & SET CONFIGURATION
 # ############################################################################
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import RandomizedSearchCV
 
 import pandas as pd
 
+from keras.layers import BatchNormalization
+from keras.wrappers.scikit_learn import KerasRegressor
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.metrics import accuracy
+
+
 # FUNCTIONS
 # ############################################################################
-# Function created by Gale Proulx
+#Gale
 def import_train_test(filename: str, feature: str, train_size=0.33):
     df = pd.read_csv(filename)
     target = df[feature]
     features = df.drop([feature, 'Average In-District Expenditures per Pupil']
                        , axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(features, target, 
+    X_train, X_test, y_train, y_test = train_test_split(features, target,
                                                         test_size=train_size)
-    
+
     return X_train, X_test, y_train, y_test
-    
+
+
+#Finn
+def build_model(nn=16, nl=5, loss="mean_squared_error", optimizer="rmsprop", activation="relu"):
+    model = Sequential()
+    model.add(Dense(units=55, input_dim=55, activation="relu"))
+    model.add(BatchNormalization())
+
+    model.add(Dense(16, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dense(16, activation="relu"))
+    model.add(BatchNormalization())
+
+    model.add(Dense(1, activation="linear"))
+
+    model.compile(optimizer="Adam", loss="mean_squared_error", metrics=['mean_squared_error'])
+    return model
+
+
+#Finn
+def hyperparameter_tuning(X_train, y_train):
+    model = KerasRegressor(build_fn=build_model, verbose=2)
+    params = {'activation': ['relu', 'tanh', 'elu', 'softmax', 'selu'], 'batch_size': [24, 32, 128, 256],
+              'epochs': [25, 50, 100, 200], 'nl': [1, 2, 5, 9],
+              'nn': [20, 60, 128, 256], 'loss': ['mean_squared_error', 'huber_loss', 'binary_crossentropy',
+                                                 'categorical_crossentropy'],
+              'optimizer': ['SGD', 'RMSprop', 'Adagrad', 'Adam', 'Adamax']}
+
+    random_search = RandomizedSearchCV(model, param_distributions=params, cv=3, n_jobs=-1, verbose=1)
+    fitted_model = random_search.fit(X_train, y_train, verbose=2)
+    return fitted_model
+
+
 # MAIN
 # ############################################################################
+#Gale and Finn
 def main() -> None:
-    # Train-test-split created by Gale Proulx
-    X_train, X_test, y_train, y_test = \
-        import_train_test('standardized_data.csv', 
+    X_train, X_test, y_train, y_test = import_train_test('cleaned_data.csv',
                           'Average Expenditures per Pupil', train_size=0.50)
 
-    # The rest of main created by Finn Jensen
-    print("Finn's Code Here...")
+
+    #model = hyperparameter_tuning(X_train, y_train)
+    model = build_model()
+    model.fit(X_train, y_train, verbose=1, epochs=1500)
+
+    y_pred = model.predict(X_test)
+
+    print("Model Info")
+    print("-------------------------------------------------")
+    print('Accuracy: {}'.format(model.evaluate(X_test, y_test)))
+    #print("Tuned Model Parameters: {}".format(model.best_params_))
+    print("-------------------------------------------------")
 
 if __name__ == "__main__":
     main()
